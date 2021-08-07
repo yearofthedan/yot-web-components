cc12d3444c7ad143e50567e0d7818d9e76191cd0
Dan Mutton <10258529+yearofthedan@users.noreply.github.com>
2021-08-07 12:15:41 +1000

    refactor: use playwright and remove pupeteer dep

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,31 +1,17 @@
-import { puppeteerLauncher } from '@web/test-runner-puppeteer';
+import { playwrightLauncher } from '@web/test-runner-playwright';
 
 export default /** @type {import("@web/test-runner").TestRunnerConfig} */ ({
   files: [
     'dist/**/*.test.js',
     'dist/**/*.spec.js'
   ],
-  // nodeResolve: true,
-  /** Compile JS for older browsers. Requires @web/dev-server-esbuild plugin */
-  // esbuildTarget: 'auto',
 
   /** Confgure bare import resolve plugin */
   nodeResolve: {
     exportConditions: ['browser', 'development']
   },
 
-  /** Amount of browsers to run concurrently */
-  // concurrentBrowsers: 2,
-
-  /** Amount of test files per browser to test concurrently */
-  // concurrency: 1,
-
-  /** Browsers to run tests on */
   browsers: [
-    puppeteerLauncher(),
-    //   playwrightLauncher({ product: 'firefox' }),
-    //   playwrightLauncher({ product: 'webkit' }),
+      playwrightLauncher({ product: 'webkit' }),
   ],
-
-  // See documentation for all available options
 });
